776da7ecab2e16ebc8ec3d66f4d204ee16aedfa8
Smith Qiang <smith.xue@foxmail.com>
2025-05-08 21:30:07 +0800

    构建命令gen-frontend-auth-page生成的样板代码

---
+++ app/frontend/biz/handler/auth/auth_service.go
@@ -0,0 +1,33 @@
+package auth
+
+import (
+	"context"
+
+	"github.com/cloudwego/gomall/app/frontend/biz/service"
+	"github.com/cloudwego/gomall/app/frontend/biz/utils"
+	auth "github.com/cloudwego/gomall/app/frontend/hertz_gen/frontend/auth"
+	common "github.com/cloudwego/gomall/app/frontend/hertz_gen/frontend/common"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
+// Login .
+// @router /auth/login [POST]
+func Login(ctx context.Context, c *app.RequestContext) {
+	var err error
+	var req auth.LoginReq
+	err = c.BindAndValidate(&req)
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+
+	resp := &common.Empty{}
+	resp, err = service.NewLoginService(ctx, c).Run(&req)
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+
+	utils.SendSuccessResponse(ctx, c, consts.StatusOK, resp)
+}
