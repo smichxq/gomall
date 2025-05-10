c1c2c78135543c5f9e50c722eea2ad19839dac6d
Smith Qiang <smith.xue@foxmail.com>
2025-05-10 18:37:47 +0800

    新增单元测试

--- app/user/biz/service/login_test.go
+++ app/user/biz/service/login_test.go
@@ -2,20 +2,36 @@ package service
 
 import (
 	"context"
+	"os"
 	"testing"
+
+	"github.com/cloudwego/gomall/app/user/biz/dal/mysql"
 	user "github.com/cloudwego/gomall/rpc_gen/kitex_gen/user"
 )
 
 func TestLogin_Run(t *testing.T) {
+	// 临时测试环境变量
+	os.Setenv("MYSQL_USER", "root")
+	os.Setenv("MYSQL_PASSWORD", "123")
+	os.Setenv("MYSQL_HOST", "192.168.3.6")
+	os.Setenv("MYSQL_PORT", "3306")
+	os.Setenv("MYSQL_DATABASE", "user")
+
+	mysql.InitUnitTest()
+
 	ctx := context.Background()
+
 	s := NewLoginService(ctx)
 	// init req and assert value
 
-	req := &user.LoginReq{}
+	req := &user.LoginReq{
+		Email:    "example@aaa.com1",
+		Password: "123asd",
+	}
 	resp, err := s.Run(req)
 	t.Logf("err: %v", err)
+
 	t.Logf("resp: %v", resp)
 
 	// todo: edit your unit test
-
 }
