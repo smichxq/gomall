563ff9b7b8c88c93fa3072db0dbd4f752b32b422
Smith Qiang <smith.xue@foxmail.com>
2025-05-10 17:35:08 +0800

    why: 单元测试获取DB时读取配置文件的路径只能是当前目录的conf how: 为单元测试新增适配方法

--- app/user/biz/dal/mysql/init.go
+++ app/user/biz/dal/mysql/init.go
@@ -55,3 +55,45 @@ func Init() {
 
 	fmt.Println(v)
 }
+
+// 适配单元测试中获取目录的问题
+func InitUnitTest() {
+	// 从环境变量获取MySQL配置
+	// 将环境变量注入到配置文件
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DATABASE"),
+	)
+	fmt.Println(dsn)
+
+	// 加载MySQL配置
+	DB, err = gorm.Open(mysql.Open(dsn),
+		&gorm.Config{
+			PrepareStmt:            true,
+			SkipDefaultTransaction: true,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	type Version struct {
+		Version string
+	}
+
+	var v Version
+
+	err = DB.Raw("select version() as version").Scan(&v).Error
+	if err != nil {
+		panic(err)
+	}
+
+	// 自动迁移
+	// DB.AutoMigrate(&model.User{})
+
+	fmt.Println(v)
+}
