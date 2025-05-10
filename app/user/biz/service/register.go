d7fd415850d00282a2079f5dfa4b8bd2b6155acd
Smith Qiang <smith.xue@foxmail.com>
2025-05-10 16:44:54 +0800

    新增业务逻辑

--- app/user/biz/service/register.go
+++ app/user/biz/service/register.go
@@ -2,7 +2,11 @@ package service
 
 import (
 	"context"
+	"errors"
+
+	"github.com/cloudwego/biz-demo/gomall/demo/demo_proto/biz/model"
 	user "github.com/cloudwego/gomall/rpc_gen/kitex_gen/user"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type RegisterService struct {
@@ -16,5 +20,23 @@ func NewRegisterService(ctx context.Context) *RegisterService {
 func (s *RegisterService) Run(req *user.RegisterReq) (resp *user.RegisterResp, err error) {
 	// Finish your business logic.
 
-	return
+	// 校验
+	if req.Password != req.PasswordConfirm {
+		return nil, errors.New("password not match")
+	}
+
+	// 加密
+	passwdHashed, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	// 实例化
+	newUser := &model.User{
+		Email:    req.Email,
+		PassWord: string(passwdHashed),
+	}
+
+	// 返回id
+	return &user.RegisterResp{UserId: int32(newUser.ID)}, nil
 }
