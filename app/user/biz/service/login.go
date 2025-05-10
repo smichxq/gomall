09d57fc698d026cb98189430a412413da4ef5f71
Smith Qiang <smith.xue@foxmail.com>
2025-05-10 18:37:33 +0800

    新增登录业务逻辑

--- app/user/biz/service/login.go
+++ app/user/biz/service/login.go
@@ -2,7 +2,13 @@ package service
 
 import (
 	"context"
+	"errors"
+	"fmt"
+
+	"github.com/cloudwego/gomall/app/user/biz/dal/mysql"
+	"github.com/cloudwego/gomall/app/user/biz/model"
 	user "github.com/cloudwego/gomall/rpc_gen/kitex_gen/user"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type LoginService struct {
@@ -16,5 +22,31 @@ func NewLoginService(ctx context.Context) *LoginService {
 func (s *LoginService) Run(req *user.LoginReq) (resp *user.LoginResp, err error) {
 	// Finish your business logic.
 
-	return
+	// 获取请求的用户名
+	userEmail, userPasswd := req.Email, req.Password
+
+	fmt.Println(userEmail, userPasswd)
+
+	if userEmail == "" || userPasswd == "" {
+		return nil, errors.New("email or passwd is empty")
+	}
+
+	// 查询记录
+	user_row, err := model.SelectByEmail(mysql.DB, userEmail)
+	if err != nil {
+		return nil, err
+	}
+
+	// 加密
+	passwdHashed, err := bcrypt.GenerateFromPassword([]byte(userPasswd), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	// 比对
+	if string(passwdHashed) != user_row.PasswordHashed {
+		return nil, err
+	}
+
+	return &user.LoginResp{UserId: int32(user_row.ID)}, nil
 }
