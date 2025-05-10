020d448606b59c9ac425c5c5bd86d99c09a3e890
Smith Qiang <smith.xue@foxmail.com>
2025-05-10 08:37:32 +0800

    配置加载方式改为从环境变量读取

--- app/user/biz/dal/mysql/init.go
+++ app/user/biz/dal/mysql/init.go
@@ -1,6 +1,9 @@
 package mysql
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/cloudwego/gomall/app/user/conf"
 
 	"gorm.io/driver/mysql"
@@ -13,7 +16,20 @@ var (
 )
 
 func Init() {
-	DB, err = gorm.Open(mysql.Open(conf.GetConf().MySQL.DSN),
+	// 从环境变量获取MySQL配置
+	// 将环境变量注入到配置文件
+	dsn := fmt.Sprintf(
+		conf.GetConf().MySQL.DSN,
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DATABASE"),
+	)
+	fmt.Println(dsn)
+
+	// 加载MySQL配置
+	DB, err = gorm.Open(mysql.Open(dsn),
 		&gorm.Config{
 			PrepareStmt:            true,
 			SkipDefaultTransaction: true,
@@ -22,4 +38,20 @@ func Init() {
 	if err != nil {
 		panic(err)
 	}
+
+	type Version struct {
+		Version string
+	}
+
+	var v Version
+
+	err = DB.Raw("select version() as version").Scan(&v).Error
+	if err != nil {
+		panic(err)
+	}
+
+	// 自动迁移
+	// DB.AutoMigrate(&model.User{})
+
+	fmt.Println(v)
 }
