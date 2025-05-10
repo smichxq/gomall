810d7bec0c618ea3b6b1e8fcaefac04f32ec98d8
Smith Qiang <smith.xue@foxmail.com>
2025-05-10 10:10:01 +0800

    修改字段适配mysql5.7

--- app/user/biz/model/user.go
+++ app/user/biz/model/user.go
@@ -4,11 +4,16 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Email          string `gorm:"uniqueIndex"`
-	PasswordHashed string `grom:"type:varchar(255) not null"`
+	Email          string `gorm:"type:varchar(255);uniqueIndex"`
+	PasswordHashed string `grom:"type:varchar(255);not null"`
 }
 
 // 设置表名
 func (User) TableName() string {
 	return "user"
+}
+
+// CRUD
+func Create(db *gorm.DB, user *User) error {
+	return db.Create(user).Error
 }
