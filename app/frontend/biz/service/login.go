2adde757edca0aa75bebc419347524d12d2fa6c5
Smith Qiang <smith.xue@foxmail.com>
2025-05-10 21:03:19 +0800

    service调用rpc客户端

--- app/frontend/biz/service/login.go
+++ app/frontend/biz/service/login.go
@@ -5,6 +5,8 @@ import (
 	"log"
 
 	auth "github.com/cloudwego/gomall/app/frontend/hertz_gen/frontend/auth"
+	"github.com/cloudwego/gomall/app/frontend/infra/rpc"
+	"github.com/cloudwego/gomall/rpc_gen/kitex_gen/user"
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/hertz-contrib/sessions"
 )
@@ -25,9 +27,18 @@ func (h *LoginService) Run(req *auth.LoginReq) (redirect string, err error) {
 	//}()
 	// todo edit your code
 
+	// 使用rpc调用服务端(app/user)
+	resp, err := rpc.UserClient.Login(h.Context, &user.LoginReq{
+		Email:    req.Email,
+		Password: req.Password,
+	})
+	if err != nil {
+		return "", err
+	}
+
 	session := sessions.Default(h.RequestContext)
 
-	session.Set("user_id", 1)
+	session.Set("user_id", resp.UserId)
 
 	err = session.Save()
 	if err != nil {
--- app/frontend/biz/service/register.go
+++ app/frontend/biz/service/register.go
@@ -6,6 +6,8 @@ import (
 
 	auth "github.com/cloudwego/gomall/app/frontend/hertz_gen/frontend/auth"
 	common "github.com/cloudwego/gomall/app/frontend/hertz_gen/frontend/common"
+	"github.com/cloudwego/gomall/app/frontend/infra/rpc"
+	"github.com/cloudwego/gomall/rpc_gen/kitex_gen/user"
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/hertz-contrib/sessions"
 )
@@ -25,9 +27,20 @@ func (h *RegisterService) Run(req *auth.RegisterReq) (resp *common.Empty, err er
 	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
 	//}()
 	// todo edit your code
+
+	// 使用rpc调用服务端(app/user)
+	respp, err := rpc.UserClient.Register(h.Context, &user.RegisterReq{
+		Email:           req.Email,
+		Password:        req.Password,
+		PasswordConfirm: req.PasswordConfirm,
+	})
+	if err != nil {
+		return nil, err
+	}
+
 	session := sessions.Default(h.RequestContext)
 
-	session.Set("user_id", 1)
+	session.Set("user_id", respp.UserId)
 
 	err = session.Save()
 	if err != nil {
