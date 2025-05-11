fd482d70a4a693b1640a44ad9e9a4ca18d5ae7fe
Smith Qiang <smith.xue@foxmail.com>
2025-05-11 11:12:24 +0800

    修改dal

--- app/product/biz/dal/mysql/init.go
+++ app/product/biz/dal/mysql/init.go
@@ -1,6 +1,10 @@
 package mysql
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/cloudwego/gomall/app/product/biz/model"
 	"github.com/cloudwego/gomall/app/product/conf"
 
 	"gorm.io/driver/mysql"
@@ -13,7 +17,20 @@ var (
 )
 
 func Init() {
-	DB, err = gorm.Open(mysql.Open(conf.GetConf().MySQL.DSN),
+	// 从环境变量获取MySQL配置
+	// 将环境变量注入到配置文件
+	dsn := fmt.Sprintf(
+		conf.GetConf().MySQL.DSN,
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DATABASE"),
+	)
+	fmt.Println(dsn)
+
+	// 加载MySQL配置
+	DB, err = gorm.Open(mysql.Open(dsn),
 		&gorm.Config{
 			PrepareStmt:            true,
 			SkipDefaultTransaction: true,
@@ -21,5 +38,85 @@ func Init() {
 	)
 	if err != nil {
 		panic(err)
+	}
+
+	type Version struct {
+		Version string
+	}
+
+	var v Version
+
+	err = DB.Raw("select version() as version").Scan(&v).Error
+	if err != nil {
+		panic(err)
+	}
+
+	// 自动迁移
+	migrate()
+
+	fmt.Println(v)
+}
+
+// 适配单元测试中获取目录的问题
+func InitUnitTest() {
+	// 从环境变量获取MySQL配置
+	// 将环境变量注入到配置文件
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DATABASE"),
+	)
+	fmt.Println(dsn)
+
+	// 加载MySQL配置
+	DB, err = gorm.Open(mysql.Open(dsn),
+		&gorm.Config{
+			PrepareStmt:            true,
+			SkipDefaultTransaction: true,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	type Version struct {
+		Version string
+	}
+
+	var v Version
+
+	err = DB.Raw("select version() as version").Scan(&v).Error
+	if err != nil {
+		panic(err)
+	}
+	migrate()
+
+	fmt.Println(v)
+}
+
+func migrate() {
+	if os.Getenv("GO_ENV") != "online" {
+		// 检查数据库中是否已存在表
+		needDemoData := !DB.Migrator().HasTable(&model.Product{})
+
+		// 自动迁移数据库表结构
+		// 根据模型定义创建或更新表结构
+		// 如果存在则尝试添加缺失字段、索引等，但不会删除字段或索引
+		// 多个模型可以一次性迁移
+		// 也会创建关联表
+		DB.AutoMigrate(
+			&model.Product{},
+			&model.Category{},
+		)
+
+		if needDemoData {
+			DB.Exec("INSERT INTO `product`.`category` VALUES (1,'2023-12-06 15:05:06','2023-12-06 15:05:06','T-Shirt','T-Shirt'),(2,'2023-12-06 15:05:06','2023-12-06 15:05:06','Sticker','Sticker')")
+			DB.Exec("INSERT INTO `product`.`product` VALUES ( 1, '2023-12-06 15:26:19', '2023-12-09 22:29:10', 'Notebook', 'The cloudwego notebook is a highly efficient and feature-rich notebook designed to meet all your note-taking needs. ', '/static/image/notebook.jpeg', 9.90 ), ( 2, '2023-12-06 15:26:19', '2023-12-09 22:29:10', 'Mouse-Pad', 'The cloudwego mouse pad is a premium-grade accessory designed to enhance your computer usage experience. ', '/static/image/mouse-pad.jpeg', 8.80 ), ( 3, '2023-12-06 15:26:19', '2023-12-09 22:31:20', 'T-Shirt', 'The cloudwego t-shirt is a stylish and comfortable clothing item that allows you to showcase your fashion sense while enjoying maximum comfort.', '/static/image/t-shirt.jpeg', 6.60 ), ( 4, '2023-12-06 15:26:19', '2023-12-09 22:31:20', 'T-Shirt', 'The cloudwego t-shirt is a stylish and comfortable clothing item that allows you to showcase your fashion sense while enjoying maximum comfort.', '/static/image/t-shirt-1.jpeg', 2.20 ), ( 5, '2023-12-06 15:26:19', '2023-12-09 22:32:35', 'Sweatshirt', 'The cloudwego Sweatshirt is a cozy and fashionable garment that provides warmth and style during colder weather.', '/static/image/sweatshirt.jpeg', 1.10 ), ( 6, '2023-12-06 15:26:19', '2023-12-09 22:31:20', 'T-Shirt', 'The cloudwego t-shirt is a stylish and comfortable clothing item that allows you to showcase your fashion sense while enjoying maximum comfort.', '/static/image/t-shirt-2.jpeg', 1.80 ), ( 7, '2023-12-06 15:26:19', '2023-12-09 22:31:20', 'mascot', 'The cloudwego mascot is a charming and captivating representation of the brand, designed to bring joy and a playful spirit to any environment.', '/static/image/logo.jpg', 4.80 )")
+			DB.Exec("INSERT INTO `product`.`product_category` (product_id,category_id) VALUES ( 1, 2 ), ( 2, 2 ), ( 3, 1 ), ( 4, 1 ), ( 5, 1 ), ( 6, 1 ),( 7, 2 )")
+
+		}
 	}
 }
