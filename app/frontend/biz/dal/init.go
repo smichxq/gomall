5008f1cdfa36c921c264ec6a20c91218321e1a83
Smith Qiang <smith.xue@foxmail.com>
2025-05-11 17:22:23 +0800

    删除无用代码

--- app/frontend/biz/dal/init.go
+++ app/frontend/biz/dal/init.go
@@ -1,11 +1,10 @@
 package dal
 
 import (
-	"github.com/cloudwego/gomall/app/frontend/biz/dal/mysql"
 	"github.com/cloudwego/gomall/app/frontend/biz/dal/redis"
 )
 
 func Init() {
 	redis.Init()
-	mysql.Init()
+	// mysql.Init()
 }
