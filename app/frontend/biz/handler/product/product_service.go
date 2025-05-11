e4dbac0426b5adf683d7cef7bac25c94944d40e4
Smith Qiang <smith.xue@foxmail.com>
2025-05-11 16:07:23 +0800

    生成product-page接口

---
+++ app/frontend/biz/handler/product/product_service.go
@@ -0,0 +1,54 @@
+package product
+
+import (
+	"context"
+
+	"github.com/cloudwego/gomall/app/frontend/biz/service"
+	"github.com/cloudwego/gomall/app/frontend/biz/utils"
+	common "github.com/cloudwego/gomall/app/frontend/hertz_gen/frontend/common"
+	product "github.com/cloudwego/gomall/app/frontend/hertz_gen/frontend/product"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
+// GetProduct .
+// @router /product [GET]
+func GetProduct(ctx context.Context, c *app.RequestContext) {
+	var err error
+	var req product.ProductReq
+	err = c.BindAndValidate(&req)
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+
+	resp := &common.Empty{}
+	resp, err = service.NewGetProductService(ctx, c).Run(&req)
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+
+	utils.SendSuccessResponse(ctx, c, consts.StatusOK, resp)
+}
+
+// SearchProducts .
+// @router /search [GET]
+func SearchProducts(ctx context.Context, c *app.RequestContext) {
+	var err error
+	var req product.SearchProductsReq
+	err = c.BindAndValidate(&req)
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+
+	resp := &common.Empty{}
+	resp, err = service.NewSearchProductsService(ctx, c).Run(&req)
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+
+	utils.SendSuccessResponse(ctx, c, consts.StatusOK, resp)
+}
---
+++ app/frontend/biz/service/search_products.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	"context"
+
+	common "github.com/cloudwego/gomall/app/frontend/hertz_gen/frontend/common"
+	product "github.com/cloudwego/gomall/app/frontend/hertz_gen/frontend/product"
+	"github.com/cloudwego/hertz/pkg/app"
+)
+
+type SearchProductsService struct {
+	RequestContext *app.RequestContext
+	Context        context.Context
+}
+
+func NewSearchProductsService(Context context.Context, RequestContext *app.RequestContext) *SearchProductsService {
+	return &SearchProductsService{RequestContext: RequestContext, Context: Context}
+}
+
+func (h *SearchProductsService) Run(req *product.SearchProductsReq) (resp *common.Empty, err error) {
+	//defer func() {
+	// hlog.CtxInfof(h.Context, "req = %+v", req)
+	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
+	//}()
+	// todo edit your code
+	return
+}
