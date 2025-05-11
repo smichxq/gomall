b2689a4f262f1d77071fcc55f387a70f24064ae9
Smith Qiang <smith.xue@foxmail.com>
2025-05-11 12:49:55 +0800

    适配consul

--- app/product/main.go
+++ app/product/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"net"
+	"net/http"
 	"time"
 
 	"github.com/cloudwego/gomall/app/product/biz/dal"
@@ -10,13 +12,19 @@ import (
 	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
+	consulapi "github.com/hashicorp/consul/api"
+
 	kitexlogrus "github.com/kitex-contrib/obs-opentelemetry/logging/logrus"
+	consul "github.com/kitex-contrib/registry-consul"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func main() {
 	opts := kitexInit()
+
+	// 健康检查
+	go StartHealthCheckServer(":8897")
 
 	svr := productcatalogservice.NewServer(new(ProductCatalogServiceImpl), opts...)
 
@@ -42,6 +50,23 @@ func kitexInit() (opts []server.Option) {
 		ServiceName: conf.GetConf().Kitex.Service,
 	}))
 
+	// build a consul register with the consul client
+	// 读取配置文件中的注册中心地址(单节点)
+	r, err := consul.NewConsulRegister(conf.GetConf().Registry.RegistryAddress[0], consul.WithCheck(&consulapi.AgentServiceCheck{
+		HTTP:                           "http://192.168.3.6:8897/health",
+		Interval:                       "1s",
+		Timeout:                        "1s",
+		DeregisterCriticalServiceAfter: "1m",
+	}))
+	// r, err := consul.NewConsulRegister(conf.GetConf().Registry.RegistryAddress[0])
+	if err != nil {
+		log.Fatal("NewConsulRegister", err)
+		return
+	}
+
+	// 组件注册到服务
+	opts = append(opts, server.WithRegistry(r))
+
 	// klog
 	logger := kitexlogrus.NewLogger()
 	klog.SetLogger(logger)
@@ -60,4 +85,17 @@ func kitexInit() (opts []server.Option) {
 		asyncWriter.Sync()
 	})
 	return
+}
+
+// 健康监测接口
+func StartHealthCheckServer(addr string) {
+	go func() {
+		http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("ok"))
+		})
+		if err := http.ListenAndServe(addr, nil); err != nil {
+			log.Fatalf("health check server error: %v", err)
+		}
+	}()
 }
