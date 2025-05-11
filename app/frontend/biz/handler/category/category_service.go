d54b89a8678082ce6a3d19a14246409ea4ef5e04
Smith Qiang <smith.xue@foxmail.com>
2025-05-11 16:06:40 +0800

    生成category-page接口

---
+++ app/frontend/biz/handler/category/category_service.go
@@ -0,0 +1,33 @@
+package category
+
+import (
+	"context"
+
+	"github.com/cloudwego/gomall/app/frontend/biz/service"
+	"github.com/cloudwego/gomall/app/frontend/biz/utils"
+	category "github.com/cloudwego/gomall/app/frontend/hertz_gen/frontend/category"
+	common "github.com/cloudwego/gomall/app/frontend/hertz_gen/frontend/common"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
+// Category .
+// @router /category/:category [GET]
+func Category(ctx context.Context, c *app.RequestContext) {
+	var err error
+	var req category.CategoryReq
+	err = c.BindAndValidate(&req)
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+
+	resp := &common.Empty{}
+	resp, err = service.NewCategoryService(ctx, c).Run(&req)
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+
+	utils.SendSuccessResponse(ctx, c, consts.StatusOK, resp)
+}
