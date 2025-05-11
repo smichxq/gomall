787858c746b60285bf2f93b5426140eb93b53b10
Smith Qiang <smith.xue@foxmail.com>
2025-05-11 16:55:53 +0800

    新增客户端调用

--- app/frontend/infra/rpc/client.go
+++ app/frontend/infra/rpc/client.go
@@ -5,13 +5,15 @@ import (
 
 	"github.com/cloudwego/gomall/app/frontend/conf"
 	frontendUtils "github.com/cloudwego/gomall/app/frontend/utils"
+	"github.com/cloudwego/gomall/rpc_gen/kitex_gen/product/productcatalogservice"
 	"github.com/cloudwego/gomall/rpc_gen/kitex_gen/user/userservice"
 	"github.com/cloudwego/kitex/client"
 	consul "github.com/kitex-contrib/registry-consul"
 )
 
 var (
-	UserClient userservice.Client
+	UserClient    userservice.Client
+	ProductClient productcatalogservice.Client
 
 	once sync.Once
 )
@@ -19,6 +21,7 @@ var (
 func Init() {
 	once.Do(func() {
 		initUserClient()
+		initProductcatalogserviceClient()
 	})
 }
 
@@ -29,5 +32,15 @@ func initUserClient() {
 
 	// 使用对应的IDL客户端
 	UserClient, err = userservice.NewClient("user", client.WithResolver(r))
+	frontendUtils.MustHandleErr(err)
+}
+
+func initProductcatalogserviceClient() {
+	// 客户端从 Consul 获取服务实例列表
+	r, err := consul.NewConsulResolver(conf.GetConf().Registry.RegistryAddress[0])
+	frontendUtils.MustHandleErr(err)
+
+	// 使用对应的IDL客户端
+	ProductClient, err = productcatalogservice.NewClient("product", client.WithResolver(r))
 	frontendUtils.MustHandleErr(err)
 }
