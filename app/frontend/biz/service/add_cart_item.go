950b8798eb6826a5579490fd0ee6ee9ff1a0d403
Smith Qiang <smith.xue@foxmail.com>
2025-05-12 17:22:59 +0800

    渲染端逻辑

--- app/frontend/biz/service/add_cart_item.go
+++ app/frontend/biz/service/add_cart_item.go
@@ -5,7 +5,12 @@ import (
 
 	cart "github.com/cloudwego/gomall/app/frontend/hertz_gen/frontend/cart"
 	common "github.com/cloudwego/gomall/app/frontend/hertz_gen/frontend/common"
+	"github.com/cloudwego/gomall/app/frontend/infra/rpc"
+	frontendUtils "github.com/cloudwego/gomall/app/frontend/utils"
+	rpccart "github.com/cloudwego/gomall/rpc_gen/kitex_gen/cart"
+
 	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
 )
 
 type AddCartItemService struct {
@@ -18,10 +23,20 @@ func NewAddCartItemService(Context context.Context, RequestContext *app.RequestC
 }
 
 func (h *AddCartItemService) Run(req *cart.AddCartItemReq) (resp *common.Empty, err error) {
-	//defer func() {
-	// hlog.CtxInfof(h.Context, "req = %+v", req)
-	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
-	//}()
+	defer func() {
+		hlog.CtxInfof(h.Context, "req = %+v", req)
+		hlog.CtxInfof(h.Context, "resp = %+v", resp)
+	}()
 	// todo edit your code
+	_, err = rpc.CartClient.AddItem(h.Context, &rpccart.AddItemReq{
+		UserId: uint32(frontendUtils.GetUserIdFromCtx(h.Context)),
+		Item: &rpccart.CartItem{
+			ProductId: req.ProductId,
+			Quantity:  uint32(req.ProductNum),
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
 	return
 }
--- app/frontend/biz/service/get_cart.go
+++ app/frontend/biz/service/get_cart.go
@@ -2,9 +2,16 @@ package service
 
 import (
 	"context"
+	"strconv"
 
 	common "github.com/cloudwego/gomall/app/frontend/hertz_gen/frontend/common"
+	"github.com/cloudwego/gomall/app/frontend/infra/rpc"
+	frontendUtils "github.com/cloudwego/gomall/app/frontend/utils"
+	"github.com/cloudwego/gomall/rpc_gen/kitex_gen/cart"
+	"github.com/cloudwego/gomall/rpc_gen/kitex_gen/product"
 	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/kitex/pkg/kerrors"
 )
 
 type GetCartService struct {
@@ -16,11 +23,44 @@ func NewGetCartService(Context context.Context, RequestContext *app.RequestConte
 	return &GetCartService{RequestContext: RequestContext, Context: Context}
 }
 
-func (h *GetCartService) Run(req *common.Empty) (resp *common.Empty, err error) {
+func (h *GetCartService) Run(req *common.Empty) (resp map[string]any, err error) {
 	//defer func() {
 	// hlog.CtxInfof(h.Context, "req = %+v", req)
 	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
 	//}()
 	// todo edit your code
-	return
+	cartResp, err := rpc.CartClient.GetCart(h.Context, &cart.GetCartReq{UserId: uint32(frontendUtils.GetUserIdFromCtx(h.Context))})
+	if err != nil {
+		frontendUtils.MustHandleErr(err)
+		return nil, kerrors.NewGRPCBizStatusError(400020, "Cart client invoke fail")
+	}
+
+	var items []map[string]string
+	var total float64
+	for _, item := range cartResp.Items {
+		productResp, err := rpc.ProductClient.GetProduct(h.Context, &product.GetProductReq{Id: item.ProductId})
+		if err != nil {
+			continue
+		}
+		p := productResp.Product
+		items = append(items, map[string]string{
+			"Name":        p.Name,
+			"Description": p.Description,
+			"Price":       strconv.FormatFloat(func() float64 { price, _ := strconv.ParseFloat(p.Price, 64); return price }(), 'f', 2, 64),
+			"Picture":     p.Picture,
+			"Qty":         strconv.Itoa(int(item.Quantity)),
+		})
+		price, err := strconv.ParseFloat(p.Price, 64)
+		if err != nil {
+			continue
+		}
+		total += price * float64(item.Quantity)
+
+	}
+
+	return utils.H{
+		"title": "Cart",
+		"items": items,
+		"total": strconv.FormatFloat(float64(total), 'f', 2, 64),
+	}, nil
 }
