84fb033bd5dc113f76edf5af1e43e90f7c8c73e3
Smith Qiang <smith.xue@foxmail.com>
2025-05-12 12:18:12 +0800

    新增对product微服务的调用客户端

---
+++ app/cart/infra/rpc/client.go
@@ -0,0 +1,32 @@
+package rpc
+
+import (
+	"sync"
+
+	"github.com/cloudwego/gomall/app/cart/conf"
+	cartutils "github.com/cloudwego/gomall/app/cart/utils"
+	"github.com/cloudwego/gomall/rpc_gen/kitex_gen/product/productcatalogservice"
+	"github.com/cloudwego/kitex/client"
+	consul "github.com/kitex-contrib/registry-consul"
+)
+
+var (
+	ProductClient productcatalogservice.Client
+
+	once sync.Once
+)
+
+func InitClient() {
+	once.Do(func() {
+		initProductClient()
+	})
+}
+
+func initProductClient() {
+	var opts []client.Option
+	r, err := consul.NewConsulResolver(conf.GetConf().Registry.RegistryAddress[0])
+	cartutils.MustHandleError(err)
+	opts = append(opts, client.WithResolver(r))
+	ProductClient, err = productcatalogservice.NewClient("product", opts...)
+	cartutils.MustHandleError(err)
+}
