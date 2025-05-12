94798b966dd37c10e5b2fe9cbb4b3dfc7b3cfba8
Smith Qiang <smith.xue@foxmail.com>
2025-05-12 10:59:39 +0800

    配置

--- app/cart/biz/dal/mysql/init.go
+++ app/cart/biz/dal/mysql/init.go
@@ -1,6 +1,9 @@
 package mysql
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/cloudwego/gomall/app/cart/conf"
 
 	"gorm.io/driver/mysql"
@@ -13,7 +16,20 @@ var (
 )
 
 func Init() {
-	DB, err = gorm.Open(mysql.Open(conf.GetConf().MySQL.DSN),
+	// 从环境变量获取MySQL配置
+	// 将环境变量注入到配置文件
+	dsn := fmt.Sprintf(
+		conf.GetConf().MySQL.DSN,
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DATABASE"),
+	)
+	fmt.Println(dsn)
+
+	// 加载MySQL配置
+	DB, err = gorm.Open(mysql.Open(dsn),
 		&gorm.Config{
 			PrepareStmt:            true,
 			SkipDefaultTransaction: true,
@@ -22,4 +38,64 @@ func Init() {
 	if err != nil {
 		panic(err)
 	}
+
+	type Version struct {
+		Version string
+	}
+
+	var v Version
+
+	err = DB.Raw("select version() as version").Scan(&v).Error
+	if err != nil {
+		panic(err)
+	}
+
+	// 自动迁移
+	migrate()
+
+	fmt.Println(v)
+}
+
+// 适配单元测试中获取目录的问题
+func InitUnitTest() {
+	// 从环境变量获取MySQL配置
+	// 将环境变量注入到配置文件
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DATABASE"),
+	)
+	fmt.Println(dsn)
+
+	// 加载MySQL配置
+	DB, err = gorm.Open(mysql.Open(dsn),
+		&gorm.Config{
+			PrepareStmt:            true,
+			SkipDefaultTransaction: true,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	type Version struct {
+		Version string
+	}
+
+	var v Version
+
+	err = DB.Raw("select version() as version").Scan(&v).Error
+	if err != nil {
+		panic(err)
+	}
+	migrate()
+
+	fmt.Println(v)
+}
+
+func migrate() {
+	panic("unimplemented")
 }
