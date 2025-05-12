ee6e2dbb17b11ca02f62f048a505741c8e7cbb09
Smith Qiang <smith.xue@foxmail.com>
2025-05-12 10:59:21 +0800

    1. 新增健康检查 2. 新增dal层初始化 3. 新增注册中心

--- app/cart/main.go
+++ app/cart/main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
+	"log"
 	"net"
+	"net/http"
 	"time"
 
+	"github.com/cloudwego/gomall/app/cart/biz/dal"
+	"github.com/cloudwego/gomall/app/cart/conf"
+	"github.com/cloudwego/gomall/rpc_gen/kitex_gen/cart/cartservice"
 	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
+	consulapi "github.com/hashicorp/consul/api"
 	kitexlogrus "github.com/kitex-contrib/obs-opentelemetry/logging/logrus"
-	"github.com/cloudwego/gomall/app/cart/conf"
-	"github.com/cloudwego/gomall/rpc_gen/kitex_gen/cart/cartservice"
+	consul "github.com/kitex-contrib/registry-consul"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func main() {
 	opts := kitexInit()
+
+	// 健康检查
+	go StartHealthCheckServer(":8896")
 
 	svr := cartservice.NewServer(new(CartServiceImpl), opts...)
 
@@ -26,6 +34,8 @@ func main() {
 }
 
 func kitexInit() (opts []server.Option) {
+	dal.Init()
+
 	// address
 	addr, err := net.ResolveTCPAddr("tcp", conf.GetConf().Kitex.Address)
 	if err != nil {
@@ -37,6 +47,22 @@ func kitexInit() (opts []server.Option) {
 	opts = append(opts, server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
 		ServiceName: conf.GetConf().Kitex.Service,
 	}))
+
+	// 读取配置文件中的注册中心地址(单节点)
+	r, err := consul.NewConsulRegister(conf.GetConf().Registry.RegistryAddress[0], consul.WithCheck(&consulapi.AgentServiceCheck{
+		HTTP:                           "http://192.168.3.6:8896/health",
+		Interval:                       "1s",
+		Timeout:                        "1s",
+		DeregisterCriticalServiceAfter: "1m",
+	}))
+	// r, err := consul.NewConsulRegister(conf.GetConf().Registry.RegistryAddress[0])
+	if err != nil {
+		log.Fatal("NewConsulRegister", err)
+		return
+	}
+
+	// 组件注册到服务
+	opts = append(opts, server.WithRegistry(r))
 
 	// klog
 	logger := kitexlogrus.NewLogger()
@@ -56,4 +82,17 @@ func kitexInit() (opts []server.Option) {
 		asyncWriter.Sync()
 	})
 	return
+}
+
+// 健康监测接口
+func StartHealthCheckServer(addr string) {
+	go func() {
+		http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("ok"))
+		})
+		if err := http.ListenAndServe(addr, nil); err != nil {
+			log.Fatalf("health check server error: %v", err)
+		}
+	}()
 }
