1467cabc042a269e92d8089d5e9b7e1c9fa5317e
Smith Qiang <smith.xue@foxmail.com>
2025-05-12 22:00:01 +0800

    为交易实现日志落库

--- app/payment/biz/service/charge.go
+++ app/payment/biz/service/charge.go
@@ -2,7 +2,15 @@ package service
 
 import (
 	"context"
+	"strconv"
+	"time"
+
+	"github.com/cloudwego/gomall/app/payment/biz/dal/mysql"
+	"github.com/cloudwego/gomall/app/payment/biz/model"
 	payment "github.com/cloudwego/gomall/rpc_gen/kitex_gen/payment"
+	"github.com/cloudwego/kitex/pkg/kerrors"
+	creditcard "github.com/durango/go-credit-card"
+	"github.com/google/uuid"
 )
 
 type ChargeService struct {
@@ -15,6 +23,36 @@ func NewChargeService(ctx context.Context) *ChargeService {
 // Run create note info
 func (s *ChargeService) Run(req *payment.ChargeReq) (resp *payment.ChargeResp, err error) {
 	// Finish your business logic.
+	card := creditcard.Card{
+		Number: req.CreditCard.CreditCardNumber,
+		Cvv:    strconv.Itoa(int(req.CreditCard.CreditCardCvv)),
+		Month:  strconv.Itoa(int(req.CreditCard.CreditCardExpirationMonth)),
+		Year:   strconv.Itoa(int(req.CreditCard.CreditCardExpirationYear)),
+	}
 
-	return
+	err = card.Validate(true)
+	if err != nil {
+		return nil, kerrors.NewGRPCBizStatusError(4004001, err.Error())
+	}
+
+	transacId, err := uuid.NewRandom()
+	if err != nil {
+		return nil, kerrors.NewGRPCBizStatusError(4005001, err.Error())
+	}
+
+	// 创建交易记录
+	err = model.CreatePaymentLog(mysql.DB, s.ctx, &model.PaymentLog{
+		UserId:        req.UserId,
+		OrderId:       req.OrderId,
+		TransactionId: transacId.String(),
+		Amount:        req.Amount,
+		PayAt:         time.Now(),
+	})
+	if err != nil {
+		return nil, kerrors.NewGRPCBizStatusError(4005002, err.Error())
+	}
+
+	return &payment.ChargeResp{
+		TransactionId: transacId.String(),
+	}, nil
 }
