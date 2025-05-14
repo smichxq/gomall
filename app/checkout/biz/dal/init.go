10ca97752b2395825d7baabcab5e56f60d75d97d
Smith Qiang <smith.xue@foxmail.com>
2025-05-14 10:59:45 +0800

    fix

--- app/checkout/biz/dal/init.go
+++ app/checkout/biz/dal/init.go
@@ -1,11 +1,10 @@
 package dal
 
 import (
-	"github.com/cloudwego/gomall/app/checkout/biz/dal/mysql"
 	"github.com/cloudwego/gomall/app/checkout/biz/dal/redis"
 )
 
 func Init() {
 	redis.Init()
-	mysql.Init()
+	// mysql.Init()
 }
