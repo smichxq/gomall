955af95f7922a77b8c558fccb5df46cda09dd874
Smith Qiang <smith.xue@foxmail.com>
2025-05-15 19:30:38 +0800

    适配opentelemetry

--- app/checkout/biz/dal/mysql/init.go
+++ app/checkout/biz/dal/mysql/init.go
@@ -5,6 +5,7 @@ import (
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/plugin/opentelemetry/tracing"
 )
 
 var (
@@ -20,6 +21,10 @@ func Init() {
 		},
 	)
 	if err != nil {
+		panic(err)
+	}
+	// opentelemetry存储插件
+	if err := DB.Use(tracing.NewPlugin(tracing.WithoutMetrics())); err != nil {
 		panic(err)
 	}
 }
